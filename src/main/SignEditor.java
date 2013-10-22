829d92277941bbcedd62676eb03b39e878edc7c4
Parker Miller <despondentdonkey@gmail.com>
2013-10-22 13:06:26 -0600

    Converted tabs to spaces.

--- src/main/SignEditor.java
+++ src/main/SignEditor.java
@@ -16,254 +16,255 @@ import org.bukkit.plugin.java.*;
  * 
  */
 public class SignEditor extends JavaPlugin {
-	public static Plugin plugin;
+    public static Plugin plugin;
 
-	public static String signEdit = "SignEditor_editSign";
-	public static String sign = "SignEditor_activeSign";
-	public static String signLines = "SignEditor_signLinesArray";
-	public static String prevLocation = "SignEditor_previousLocation";
+    public static String signEdit     = "SignEditor_editSign";
+    public static String sign         = "SignEditor_activeSign";
+    public static String signLines    = "SignEditor_signLinesArray";
+    public static String prevLocation = "SignEditor_previousLocation";
 
-	@Override
-	public void onEnable() {
-		plugin = this;
-		//Register events.
-		getServer().getPluginManager().registerEvents(new SignEditorEvents(), this);
-	}
+    @Override
+    public void onEnable() {
+        plugin = this;
+        //Register events.
+        getServer().getPluginManager().registerEvents(new SignEditorEvents(), this);
+    }
 
-	@Override
-	public void onDisable() {
-	}
+    @Override
+    public void onDisable() {
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player p = (Player) sender;
-			boolean editing = (boolean) getMetadata(p, signEdit, this);
-			Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            boolean editing = (boolean) getMetadata(p, signEdit, this);
+            Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
 
-			//Toggle sign edit command.
-			if (cmd.getName().equalsIgnoreCase("toggleSignEdit")) {
-				if (editing)
-					p.setMetadata(signEdit, new FixedMetadataValue(this, false));
-				else
-					p.setMetadata(signEdit, new FixedMetadataValue(this, true));
+            //Toggle sign edit command.
+            if (cmd.getName().equalsIgnoreCase("toggleSignEdit")) {
+                if (editing)
+                    p.setMetadata(signEdit, new FixedMetadataValue(this, false));
+                else
+                    p.setMetadata(signEdit, new FixedMetadataValue(this, true));
 
-				say(p, !editing ? "Editing has been enabled." : "Editing has been disabled.");
-				return true;
-			}
+                say(p, !editing ? "Editing has been enabled." : "Editing has been disabled.");
+                return true;
+            }
 
-			//Edit sign command.
-			if (cmd.getName().equalsIgnoreCase("editSign")) {
-				if (editing) {
-					if (sign != null) {
-						for (int i = 0; i < Math.min(args.length, 4); i++) {
-							String text = args[i];
+            //Edit sign command.
+            if (cmd.getName().equalsIgnoreCase("editSign")) {
+                if (editing) {
+                    if (sign != null) {
+                        for (int i = 0; i < Math.min(args.length, 4); i++) {
+                            String text = args[i];
 
-							char[] chars = text.toCharArray();
+                            char[] chars = text.toCharArray();
 
-							for (char c : chars) {
-								if (c == '_') {
-									text = text.replace(c, ' ');
-								}
-							}
+                            for (char c : chars) {
+                                if (c == '_') {
+                                    text = text.replace(c, ' ');
+                                }
+                            }
 
-							sign.setLine(i, text);
-						}
+                            sign.setLine(i, text);
+                        }
 
-						sign.update();
+                        sign.update();
 
-						return true;
-					} else {
-						say(p, "No sign is active.");
-						return true;
-					}
-				} else {
-					noSignEditMessage(p);
-					return true;
-				}
-			}
+                        return true;
+                    } else {
+                        say(p, "No sign is active.");
+                        return true;
+                    }
+                } else {
+                    noSignEditMessage(p);
+                    return true;
+                }
+            }
 
-			//Edit sign line command.
-			if (cmd.getName().equalsIgnoreCase("editSignln")) {
-				if (editing) {
-					if (sign != null) {
-						int line = Integer.parseInt(args[0]);
+            //Edit sign line command.
+            if (cmd.getName().equalsIgnoreCase("editSignln")) {
+                if (editing) {
+                    if (sign != null) {
+                        int line = Integer.parseInt(args[0]);
 
-						if (line > 0 && line < 5) {
-							StringBuilder sb = new StringBuilder();
+                        if (line > 0 && line < 5) {
+                            StringBuilder sb = new StringBuilder();
 
-							for (int i = 1; i < args.length; i++) {
-								sb.append(args[i]);
-								sb.append(" ");
-							}
+                            for (int i = 1; i < args.length; i++) {
+                                sb.append(args[i]);
+                                sb.append(" ");
+                            }
 
-							sign.setLine(line - 1, sb.toString());
-							sign.update();
-						} else {
-							say(p, "Invalid line number. Must be 1 to 4.");
-						}
+                            sign.setLine(line - 1, sb.toString());
+                            sign.update();
+                        } else {
+                            say(p, "Invalid line number. Must be 1 to 4.");
+                        }
 
-						return true;
-					} else {
-						say(p, "No sign is active.");
-						return true;
-					}
-				} else {
-					noSignEditMessage(p);
-					return true;
-				}
-			}
+                        return true;
+                    } else {
+                        say(p, "No sign is active.");
+                        return true;
+                    }
+                } else {
+                    noSignEditMessage(p);
+                    return true;
+                }
+            }
 
-			//Clear sign command.
-			if (cmd.getName().equalsIgnoreCase("clearSign")) {
-				clearActiveSign(p);
-				return true;
-			}
+            //Clear sign command.
+            if (cmd.getName().equalsIgnoreCase("clearSign")) {
+                clearActiveSign(p);
+                return true;
+            }
 
-			//Copy sign command.
-			if (cmd.getName().equalsIgnoreCase("copySign")) {
-				if (copyActiveSign(p)) {
-					say(p, "Sign has been copied.");
-				}
+            //Copy sign command.
+            if (cmd.getName().equalsIgnoreCase("copySign")) {
+                if (copyActiveSign(p)) {
+                    say(p, "Sign has been copied.");
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-			//Cut sign command.
-			if (cmd.getName().equalsIgnoreCase("cutSign")) {
-				if (copyActiveSign(p)) {
-					clearActiveSign(p);
-					say(p, "Sign has been cut.");
-				}
+            //Cut sign command.
+            if (cmd.getName().equalsIgnoreCase("cutSign")) {
+                if (copyActiveSign(p)) {
+                    clearActiveSign(p);
+                    say(p, "Sign has been cut.");
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-			//Paste sign command.
-			if (cmd.getName().equalsIgnoreCase("pasteSign")) {
-				if (editing) {
-					if (sign != null) {
-						String[] sl = (String[]) getMetadata(p, signLines, this);
+            //Paste sign command.
+            if (cmd.getName().equalsIgnoreCase("pasteSign")) {
+                if (editing) {
+                    if (sign != null) {
+                        String[] sl = (String[]) getMetadata(p, signLines, this);
 
-						for (int i = 0; i < sl.length; i++) {
-							sign.setLine(i, sl[i]);
-						}
+                        for (int i = 0; i < sl.length; i++) {
+                            sign.setLine(i, sl[i]);
+                        }
 
-						sign.update();
-						return true;
-					} else {
-						say(p, "No sign is active.");
-						return true;
-					}
-				} else {
-					noSignEditMessage(p);
-					return true;
-				}
-			}
+                        sign.update();
+                        return true;
+                    } else {
+                        say(p, "No sign is active.");
+                        return true;
+                    }
+                } else {
+                    noSignEditMessage(p);
+                    return true;
+                }
+            }
 
-			if (cmd.getName().equalsIgnoreCase("tpToSign")) {
-				if (sign != null) {
-					setMetaData(p, prevLocation, p.getLocation(), this);
-					p.teleport(sign.getLocation());
-				} else {
-					say(p, "No sign is active.");
-				}
+            if (cmd.getName().equalsIgnoreCase("tpToSign")) {
+                if (sign != null) {
+                    setMetaData(p, prevLocation, p.getLocation(), this);
+                    p.teleport(sign.getLocation());
+                } else {
+                    say(p, "No sign is active.");
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-			if (cmd.getName().equalsIgnoreCase("tpBackFromSign")) {
-				Location previousLocation = (Location) getMetadata(p, prevLocation, this);
+            if (cmd.getName().equalsIgnoreCase("tpBackFromSign")) {
+                Location previousLocation = (Location) getMetadata(p, prevLocation, this);
 
-				if (previousLocation != null) {
-					p.teleport(previousLocation);
-				} else {
-					say(p, "No location has been logged. You must first use 'tpToSign'.");
-				}
+                if (previousLocation != null) {
+                    p.teleport(previousLocation);
+                } else {
+                    say(p, "No location has been logged. You must first use 'tpToSign'.");
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-		} else {
-			sender.sendMessage("Sign Editor: You must be a player for this to work.");
-			return true;
-		}
+        } else {
+            sender.sendMessage("Sign Editor: You must be a player for this to work.");
 
-		return false;
-	}
+            return true;
+        }
 
-	public boolean copyActiveSign(Player p) {
-		boolean editing = (boolean) getMetadata(p, signEdit, this);
-		Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
+        return false;
+    }
 
-		if (editing) {
-			if (sign != null) {
-				String[] sl = new String[4];
+    public boolean copyActiveSign(Player p) {
+        boolean editing = (boolean) getMetadata(p, signEdit, this);
+        Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
 
-				for (int i = 0; i < sl.length; i++) {
-					sl[i] = sign.getLine(i);
-				}
+        if (editing) {
+            if (sign != null) {
+                String[] sl = new String[4];
 
-				p.setMetadata(signLines, new FixedMetadataValue(this, sl));
+                for (int i = 0; i < sl.length; i++) {
+                    sl[i] = sign.getLine(i);
+                }
 
-				return true;
-			} else {
-				say(p, "No sign is active.");
-				return false;
-			}
-		} else {
-			noSignEditMessage(p);
-			return false;
-		}
-	}
+                p.setMetadata(signLines, new FixedMetadataValue(this, sl));
 
-	public boolean clearActiveSign(Player p) {
-		boolean editing = (boolean) getMetadata(p, signEdit, this);
-		Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
+                return true;
+            } else {
+                say(p, "No sign is active.");
+                return false;
+            }
+        } else {
+            noSignEditMessage(p);
+            return false;
+        }
+    }
 
-		if (editing) {
-			if (sign != null) {
-				for (int i = 0; i < 4; ++i) {
-					sign.setLine(i, "");
-				}
+    public boolean clearActiveSign(Player p) {
+        boolean editing = (boolean) getMetadata(p, signEdit, this);
+        Sign sign = (Sign) getMetadata(p, SignEditor.sign, this);
 
-				sign.update();
+        if (editing) {
+            if (sign != null) {
+                for (int i = 0; i < 4; ++i) {
+                    sign.setLine(i, "");
+                }
 
-				return true;
-			} else {
-				say(p, "No sign is active.");
-				return false;
-			}
-		} else {
-			noSignEditMessage(p);
-			return false;
-		}
-	}
+                sign.update();
 
-	/**
-	 * Common message shown when editing is disabled.
-	 * 
-	 * @param s The player to send the message to.
-	 */
-	public static void noSignEditMessage(Player p) {
-		say(p, "Sign editing is not enabled. To enable, use the command: toggleSignEdit");
-	}
+                return true;
+            } else {
+                say(p, "No sign is active.");
+                return false;
+            }
+        } else {
+            noSignEditMessage(p);
+            return false;
+        }
+    }
 
-	public static void say(Player p, String s) {
-		p.sendMessage(ChatColor.GOLD + "Sign Editor: " + ChatColor.WHITE + s);
-	}
+    /**
+     * Common message shown when editing is disabled.
+     * 
+     * @param s The player to send the message to.
+     */
+    public static void noSignEditMessage(Player p) {
+        say(p, "Sign editing is not enabled. To enable, use the command: toggleSignEdit");
+    }
 
-	public static void setMetaData(Player p, String key, Object value, Plugin plugin) {
-		p.setMetadata(key, new FixedMetadataValue(plugin, value));
-	}
+    public static void say(Player p, String s) {
+        p.sendMessage(ChatColor.GOLD + "Sign Editor: " + ChatColor.WHITE + s);
+    }
 
-	public static Object getMetadata(Player player, String key, Plugin plugin) {
-		List<MetadataValue> values = player.getMetadata(key);
-		for (MetadataValue value : values) {
-			if (value.getOwningPlugin().getDescription().getName().equals(plugin.getDescription().getName())) {
-				return value.value();
-			}
-		}
-		return null;
-	}
+    public static void setMetaData(Player p, String key, Object value, Plugin plugin) {
+        p.setMetadata(key, new FixedMetadataValue(plugin, value));
+    }
+
+    public static Object getMetadata(Player player, String key, Plugin plugin) {
+        List<MetadataValue> values = player.getMetadata(key);
+        for (MetadataValue value : values) {
+            if (value.getOwningPlugin().getDescription().getName().equals(plugin.getDescription().getName())) {
+                return value.value();
+            }
+        }
+        return null;
+    }
 }
--- src/main/SignEditorEvents.java
+++ src/main/SignEditorEvents.java
@@ -13,28 +13,28 @@ import org.bukkit.metadata.*;
  */
 public class SignEditorEvents implements Listener {
 
-	@EventHandler()
-	public void onLogin(PlayerLoginEvent e) {
-		e.getPlayer().setMetadata(SignEditor.signEdit, new FixedMetadataValue(SignEditor.plugin, false));
-		e.getPlayer().setMetadata(SignEditor.signLines, new FixedMetadataValue(SignEditor.plugin, new String[4]));
-	}
+    @EventHandler()
+    public void onLogin(PlayerLoginEvent e) {
+        e.getPlayer().setMetadata(SignEditor.signEdit, new FixedMetadataValue(SignEditor.plugin, false));
+        e.getPlayer().setMetadata(SignEditor.signLines, new FixedMetadataValue(SignEditor.plugin, new String[4]));
+    }
 
-	@EventHandler()
-	public void onInteract(PlayerInteractEvent e) {
-		if ((boolean) SignEditor.getMetadata(e.getPlayer(), SignEditor.signEdit, SignEditor.plugin)) {
-			Block clickedBlock = e.getClickedBlock();
+    @EventHandler()
+    public void onInteract(PlayerInteractEvent e) {
+        if ((boolean) SignEditor.getMetadata(e.getPlayer(), SignEditor.signEdit, SignEditor.plugin)) {
+            Block clickedBlock = e.getClickedBlock();
 
-			if (clickedBlock != null) {
-				BlockState clickedBlockState = clickedBlock.getState();
+            if (clickedBlock != null) {
+                BlockState clickedBlockState = clickedBlock.getState();
 
-				if (clickedBlockState instanceof Sign) {
-					Sign s = (Sign) clickedBlockState;
-					e.getPlayer().setMetadata(SignEditor.sign, new FixedMetadataValue(SignEditor.plugin, s));
-					SignEditor.say(e.getPlayer(), "Sign active.");
-				}
-			} else {
-				SignEditor.say(e.getPlayer(), "Do not use a block to select a sign. Instead use your hand or some other item that cannot be placed.");
-			}
-		}
-	}
+                if (clickedBlockState instanceof Sign) {
+                    Sign s = (Sign) clickedBlockState;
+                    e.getPlayer().setMetadata(SignEditor.sign, new FixedMetadataValue(SignEditor.plugin, s));
+                    SignEditor.say(e.getPlayer(), "Sign active.");
+                }
+            } else {
+                SignEditor.say(e.getPlayer(), "Do not use a block to select a sign. Instead use your hand or some other item that cannot be placed.");
+            }
+        }
+    }
 }
