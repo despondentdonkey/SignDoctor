cc3d95ac254715263d871f562929b18d96db0eb3
Parker Miller <despondentdonkey@gmail.com>
2013-10-26 00:01:55 -0600

    Must have feather in hand to activate sign.

--- src/main/SignEditorEvents.java
+++ src/main/SignEditorEvents.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.bukkit.*;
 import org.bukkit.block.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.*;
@@ -27,15 +28,18 @@ public class SignEditorEvents implements Listener {
     public void onInteract(PlayerInteractEvent e) {
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
             Player p = e.getPlayer();
-            Block clickedBlock = e.getClickedBlock();
-            BlockState clickedBlockState = clickedBlock.getState();
 
-            if ((boolean) SignEditor.getMetadata(p, SignEditor.SIGN_EDIT, SignEditor.plugin)) {
-                if (clickedBlockState instanceof Sign) {
-                    Sign s = (Sign) clickedBlockState;
+            if (p.getItemInHand().getData().getItemType() == Material.FEATHER) {
+                Block clickedBlock = e.getClickedBlock();
+                BlockState clickedBlockState = clickedBlock.getState();
 
-                    p.setMetadata(SignEditor.SIGN, new FixedMetadataValue(SignEditor.plugin, s));
-                    SignEditor.say(p, "Sign active.");
+                if ((boolean) SignEditor.getMetadata(p, SignEditor.SIGN_EDIT, SignEditor.plugin)) {
+                    if (clickedBlockState instanceof Sign) {
+                        Sign s = (Sign) clickedBlockState;
+
+                        p.setMetadata(SignEditor.SIGN, new FixedMetadataValue(SignEditor.plugin, s));
+                        SignEditor.say(p, "Sign active.");
+                    }
                 }
             }
         }
